2508a5d42b70e41ee1489e5d4989247ff574a3c2
MAEDA HAJIME <anakin.maeda@gmail.com>
2015-06-19 20:30:32 +0900

    TableDetail
    Objective-C Table 表示データ 配列 Plist パターン

---
+++ TableDetailTests/TableDetailTests.m
@@ -0,0 +1,34 @@
+//
+//  TableDetailTests.m
+//  TableDetailTests
+//
+//  Created by MAEDA HAJIME on 2014/04/08.
+//  Copyright (c) 2014年 HAJIME MAEDA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableDetailTests : XCTestCase
+
+@end
+
+@implementation TableDetailTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
